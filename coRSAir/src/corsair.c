f88e0df4b80e5ab04defe615f98104ab30359508
Duvan Gonzalez <103336552+duvangonzalez-ciclo@users.noreply.github.com>
2022-07-16 12:04:46 +0200

    corsair

---
+++ coRSAir/src/corsair.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   corsair.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/13 09:52:02 by dugonzal          #+#    #+#             */
+/*   Updated: 2022/07/14 15:34:36 by ciclo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "corsair.h"
+
+/*/
+Construya la clave privada a partir de dos primos y su producto (módulo)
+y esa es la clave privada(clave cifrada).
+
+Implementación propia de una librería o conjunto de funciones en C para operar
+con enteros de gran tamaño.
+
+tengo que abrir un archivo y guardar la clave generada en un archivo.
+
+cuestion: ¿Qué es una clave privada?
+
+operacion de obtencion de dos primos y su producto (módulo)
+
+	esta haciendo una funcion que recibe como parametro dos numeros primos done
+
+
+	// Encuentra el máximo común divisor entre dos números??
+
+	porque necesito encotrar el maximo comun divisor entre los numeros primos?
+	esta funncion es recursiva, es decir que llama a si misma hasta que se obtenga
+
+	while(num2 > 0) // porque num2 > 0 // por decir algo.
+    {
+      temp = num1 % num2; //porqu
+      num1 = num2;
+      num2 = temp;
+    }
+*/
+
+int	main(void)
+{
+	int	fd;
+
+	fd = open ("keys/key_public.txt", O_WRONLY);
+	printf ("%d\n", fd);
+	close (fd);
+}
---
+++ coRSAir/src/corsair.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   corsair.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/13 09:08:34 by dugonzal          #+#    #+#             */
+/*   Updated: 2022/07/14 16:22:32 by ciclo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CORS_AIR_H
+# define CORS_AIR_H
+# include <stdio.h>
+# include <math.h>
+# include <openssl/bn.h>
+
+# include <string.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# define BUFF_SIZE 32
+
+char	*ft_strjoin(char *full, char *buffer);
+int		ft_slen(char *string, char c);
+
+#endif
