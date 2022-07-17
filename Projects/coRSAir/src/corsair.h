5da6cb8cc46f9b3f803ac28e53b48f933c95f20c
Duvan Gonzalez <103336552+duvangonzalez-ciclo@users.noreply.github.com>
2022-07-17 14:18:30 +0200

    images corsair

--- Projects/coRSAir/src/corsair.h
+++ Projects/coRSAir/src/corsair.h
@@ -5,12 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/13 09:08:34 by dugonzal          #+#    #+#             */
-/*   Updated: 2022/07/14 16:22:32 by ciclo            ###   ########.fr       */
+/*   Created: 2022/07/17 13:55:02 by ciclo             #+#    #+#             */
+/*   Updated: 2022/07/17 13:56:33 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CORS_AIR_H
+#ifndef  CORS_AIR_H
 # define CORS_AIR_H
 # include <stdio.h>
 # include <math.h>
@@ -20,8 +20,7 @@
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdlib.h>
-# define BUFF_SIZE 32
-
+//# define BUFF_SIZE 32
 char	*ft_strjoin(char *full, char *buffer);
 int		ft_slen(char *string, char c);
 
