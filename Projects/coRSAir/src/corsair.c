5da6842222c31ecc559d31eb2c772f38369bda96
Duvan Gonzalez <dugonzal@c3r6s4.42urduliz.com>
2022-07-20 21:44:59 +0200

    corsair con comandos de bash

--- Projects/coRSAir/src/corsair.c
+++ Projects/coRSAir/src/corsair.c
@@ -6,7 +6,7 @@
 /*   By: dugonzal <dugonzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 09:52:02 by dugonzal          #+#    #+#             */
-/*   Updated: 2022/07/20 20:02:24 by dugonzal         ###   ########.fr       */
+/*   Updated: 2022/07/20 21:19:02 by dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 /*/
 Construya la clave privada a partir de dos primos y su producto (módulo)
 y esa es la clave privada(clave cifrada).
-/*/
 
+
+*/
 void	ft_generate_keys(void)
 {
-	system ("openssl genrsa -out private.pem 2048");
-	system ("openssl rsa -in private.pem -pubout -out public.pem ");
-	system ("mv private.pem public.pem keys");
+	system ("openssl genrsa -out private.key 2048"); //Genera la clave privada
+	system ("openssl rsa -in private.key -pubout -out public.key "); //-pubout para generar la clave pública
+	system ("mv private.key public.key keys"); //mover claves a carpeta keys
 }
 
 int	main(void)
